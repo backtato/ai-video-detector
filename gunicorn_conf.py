e4159c1b984b0d9a622f2f251d8e4e19c0ac3f61
backtato <68750592+backtato@users.noreply.github.com>
2025-10-23 16:27:22 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -6,5 +6,3 @@ worker_class = "uvicorn.workers.UvicornWorker"
 timeout = int(os.getenv("WORKER_TIMEOUT", "120"))
 graceful_timeout = 30
 keepalive = 5
-preload_app = False  # evita import pesanti prima del fork
-loglevel = os.getenv("LOGLEVEL", "info")
