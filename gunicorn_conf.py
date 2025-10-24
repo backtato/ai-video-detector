a3bd39ae98fb41f15ab1f1ad9377704092c7ef2b
backtato <68750592+backtato@users.noreply.github.com>
2025-10-24 16:14:21 +0200

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,8 +1,11 @@
-import os
-
+# gunicorn_conf.py
+import multiprocessing
 bind = "0.0.0.0:8000"
-workers = int(os.getenv("WEB_CONCURRENCY", "2"))
+workers = max(2, multiprocessing.cpu_count() // 2)
 worker_class = "uvicorn.workers.UvicornWorker"
-timeout = int(os.getenv("WORKER_TIMEOUT", "120"))
+timeout = 180
 graceful_timeout = 30
 keepalive = 5
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
