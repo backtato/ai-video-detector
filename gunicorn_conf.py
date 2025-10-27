388fcc853497ead81f545ab5bb9fd295b1ad892e
backtato <68750592+backtato@users.noreply.github.com>
2025-10-27 23:55:39 +0100

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,7 +1,6 @@
 # gunicorn_conf.py
-import multiprocessing
 bind = "0.0.0.0:8000"
-workers = max(2, multiprocessing.cpu_count() // 2)
+workers = 2  # fisso per ridurre RAM in parallelo
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 180
 graceful_timeout = 30
