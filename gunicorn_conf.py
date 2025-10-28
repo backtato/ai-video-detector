2a2fac35eead84f0e9778dd96c0b8dd20bab9878
backtato <68750592+backtato@users.noreply.github.com>
2025-10-28 01:09:22 +0100

    Update gunicorn_conf.py

--- gunicorn_conf.py
+++ gunicorn_conf.py
@@ -1,6 +1,5 @@
-# gunicorn_conf.py
 bind = "0.0.0.0:8000"
-workers = 2  # fisso per ridurre RAM in parallelo
+workers = 2
 worker_class = "uvicorn.workers.UvicornWorker"
 timeout = 180
 graceful_timeout = 30
